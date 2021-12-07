48763ba2b3ed1bd7b5c180e80658f81e5f50813d
abhinavkrdeeps <abhikrdips@gmail.com>
2021-12-07 11:50:01 +0530

    Added Base Code

---
+++ login-signup-spring-boot/src/main/java/com/wissen/training/loginsignupspringboot/LoginSignupSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wissen.training.loginsignupspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSignupSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSignupSpringBootApplication.class, args);
+	}
+
+}
---
+++ login-signup-spring-boot/src/test/java/com/wissen/training/loginsignupspringboot/LoginSignupSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wissen.training.loginsignupspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSignupSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
